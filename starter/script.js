6bad9a101e816fdb3f8c8ba1bcb618e7a26347e7
LillyIlex <116085080+LillyIlex@users.noreply.github.com>
2023-01-20 20:43:58 +0000

    added the JS file with the current date and time from moment, and an if statement running through what hour it is, and whether that is past, present or future in the diary. Also, added a form from bootstrap with save buttons and a field for text input.

---
+++ starter/script.js
@@ -0,0 +1,63 @@
+
+
+
+// Display the current day 
+
+var datetime = null,
+        date = null;
+var textRow = document.querySelector(".textRow")
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+$(document).ready(function(){
+    datetime = $('#currentDay')
+    update();
+    setInterval(update, 1000);
+});
+
+var notTime= 15
+//Color-code each timeblock based on past, present, and future
+
+//const currentHour = moment().hour()
+//console.log(hour)
+
+/*if (currentHour === moment().hour()) {
+    textRow.addClass("present")
+    console.log("present")
+} else if (currentHour < moment().hour()) {
+    textRow.addClass("past")
+    textRow.removeClass("present")
+    console.log("grey out")
+} else {
+    textRow.addClass("future")
+    console.log("turn green")
+} */
+
+function timeChecker(currentHour, textRow) {
+    var currentHour = moment().hours()
+    if (currentHour === currentHour) {
+        textRow.addClass('present');
+        console.log('You are in the Present!');
+    } else if (currentHour > hour) {
+        console.log(' <- Elements in the Past!');
+        textRow.addClass('past');
+        textRow.removeClass('present');
+    } else {
+        console.log('<- Elements in the Future!');
+        textRow.addClass('future');
+    }
+}
+
+
+
+//Allow a user to enter an event when they click a timeblock (event.listeners on save button)
+saveBtn.addEventListener("click", )
+
+//Save the event in local storage when the save button is clicked in that timeblock (local storage)
+
+//Persist events between refreshes of a page (local storage)
+
+
+//
