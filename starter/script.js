1c688df4868e4c21ba8bc92a4aeaad56955d3cd3
LillyIlex <116085080+LillyIlex@users.noreply.github.com>
2023-01-25 23:23:14 +0000

    added click event for save button, and removed comments that are no longer needed

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,12 @@
 
-// DATE & TIME FROM MOMENT.JS
+//GLOBAL VARS
+var currentTime = moment().hours();
+var textArea = $(".text-area");
+var textBox = $(".textRow");
+var testTime = 12
+//console.log(textBox)
 
+// DATE & TIME FROM MOMENT.JS
 var datetime = null,
     date = null;
 
@@ -14,13 +20,6 @@ $(document).ready(function () {
     setInterval(update, 1000);
 });
 
-
-//GLOBAL VARS
-var currentTime = moment().hours();
-var textArea = $(".text-area");
-var textBox = $(".textRow");
-var testTime = 12
-//console.log(textBox)
 
 //Color-code each timeblock based on past, present, and future
 
@@ -39,46 +38,19 @@ for (i = 0; i < textBox.length; i++) {
     } 
 }
 
-
-/*
-var saveBtn = document.querySelector(".saveBtn")
-textArea.textContent = ;
-
-//Save the event in local storage when the save button is clicked in that timeblock (local storage)
-var textEntry = localStorage.getItem("saveBtn");
-
-//Allow a user to enter an event when they click a timeblock 
-//(event.listeners on save button)
-
-saveBtn.addEventListener("click", function() {
-   textArea.textContent = saveBtn;
-    localStorage.setItem("textEntry", textEntry);
-}
-)
-//Persist events between refreshes of a page (local storage)
-
-*/
-
-
-
-//function to compare the presave item to the text area block so the write activity gets put on the right time
-
-
 //To store the values in the textarea using local storage
+
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+ var textEntry = $("textarea").val();
+ localStorage.setItem("textEntry", textEntry);
+console.log(textEntry)
+$(".saveBtn").val(localStorage.getItem(textEntry))
+})
+
+//Persist events between refreshes of a page 
 
  //To retain the textarea details after refresh. Getting dettails from local storage
 /* $(function(){
     $("#9").val(localStorage.getItem("9am"));
-    $("#10").val(localStorage.getItem("10am"));
-    $("#11").val(localStorage.getItem("11am"));
-    $("#12").val(localStorage.getItem("12pm"));
-    $("#13").val(localStorage.getItem("1pm"));
-    $("#14").val(localStorage.getItem("2pm"));
-    $("15").val(localStorage.getItem("3pm"));
-    $("16").val(localStorage.getItem("4pm"));
-    $("17").val(localStorage.getItem("5pm"));
-}); */
-
-
-
-//event listner listens for button clicks then saves that text to the local storage 
+*/
